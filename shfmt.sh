f12dab8b7e5263976971abb92aa383d2f3e39d96
bachnxuan <bachnxuan@gmail.com>
2025-07-28 16:29:38 +0700

    refactor(shfmt): cleanup
    Signed-off-by: bintang774 <108184157+bintang774@users.noreply.github.com>

--- shfmt.sh
+++ shfmt.sh
@@ -4,6 +4,6 @@ __DIR="$(dirname "$(realpath "$0")")"
 if ! command -v shfmt &> /dev/null; then
   echo "Installing shfmt..."
   sleep 1
-  sudo apt-get -qq update && sudo apt-get -qq install -y shfmt
+  sudo apt-get update -qq && sudo apt-get install -qq shfmt
 fi
-find "$__DIR" -name "*.sh" -exec shfmt -w -i 2 -ci -sr -bn {} + -sr -bn {} +
+find "$__DIR" -name "*.sh" -exec shfmt -w -i 2 -ci -sr -bn {} +
